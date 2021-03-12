3cf56156d0119b0df48c14660cf4688851f94ee4
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-12 11:54:35 -0600

    working on dropdown

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,38 @@ import { makeLegoList } from './legos/LegoList.js';
 const navElement = document.querySelector("nav");
 
 navElement.addEventListener("click", (event) => {
-	if (event.target.id === "showBlue") {
-		filterLegos("Blue")
+	if (event.target.id === "showRed") {
+		filterLegos("Red")
 	} else if (event.target.id === "showAll") {
 		makeLegoList(useLegos())
 	}
 })
+
+navElement.addEventListener("click", (event) => {
+	if (event.target.id === "showGreen") {
+		filterLegos("Green")
+	} else if (event.target.id === "showAll") {
+		makeLegoList(useLegos())
+	}
+})
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	  var dropdowns = document.getElementsByClassName("dropdown-content");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }
 
 const filterLegos = (whatFilter) => {
 	const filterArray = useLegos().filter(singleLego => {
